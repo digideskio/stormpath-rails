9308ace8f49c5e76b9c37762023fe60f2b1230de
Boram Yoon <boram@carbonfive.com>
2013-05-22 13:32:11 -0700

    Clean up tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,18 +20,20 @@ module Stormpath
     end
 
     def obtain_test_account(opts={})
-      opts.tap do |o|
-        o['surname']    = (!opts['surname'].blank? && opts['surname']) || 'testsurname'
-        o['given_name'] = (!opts['given_name'].blank? && opts['given_name']) || 'testgivenname'
-        o['username']   = (!opts['username'].blank? && opts['username']) || 'testfoobar'
-        o['password']   = (!opts['password'].blank? && opts['password']) || 'Succ3ss!'
-        o['email']      = (!opts['email'].blank? && opts['email']) || 'test+foo+bar@example.com'
-      end
+      defaults = {
+        surname: 'testsurname',
+        given_name: 'testgivenname',
+        username: 'testfoobar',
+        password: 'Succ3ss!',
+        email: 'test+foo+bar@example'
+      }
 
       begin
-        Stormpath::Rails::Client.create_account! opts
+        Stormpath::Rails::Client.create_account! defaults.merge!(opts)
       rescue Stormpath::Error => e
-        Stormpath::Rails::Client.authenticate_account(opts['username'], opts['password'])
+        Stormpath::Rails::Client.authenticate_account(
+          opts[:username], opts[:password]
+        )
       end
     end
   end
